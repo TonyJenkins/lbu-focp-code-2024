68f68e59f4549c882881594b9185b6e1a962ad7f
Tony Jenkins <tony@tony-jenkins.co.uk>
2024-10-04 10:43:03 +0100

    Add error checks to age program

--- lectures/01/hello_age.py
+++ lectures/01/hello_age.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
 if __name__ == "__main__":
-    name = input("What is your name? ")
 
-    year_now = int(input('What is the current year? '))
-    year_born = int(input('What year were you born? '))
+    name = input('What is your name? ')
 
-    age = year_now - year_born
+    if name:
+        try:
+            year_now = int(input('What is the current year? '))
+            year_born = int(input('What year were you born? '))
 
-    print()
-    print(f"Hello, {name.title()}! It is good to meet you.")
-    print(f"This year you will be {age} years old.")
+            if year_born > year_now:
+                print('Invalid input. You cannot be born in the future.')
+            else:
+                age = year_now - year_born
+
+                print()
+                print(f'Hello, {name.title()}! It is good to meet you.')
+                print(f'This year you will be {age} years old.')
+
+        except ValueError:
+            print('Invalid input. Please enter a number.')
+
+    else:
+        print('Invalid input. Please enter a name.')
