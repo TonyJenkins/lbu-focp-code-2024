86fb137201ddd3ce549a8c3b5d11191d13eec13c
pat <p@p>
2024-10-25 12:21:04 +0100

    Minor tweaks

--- practicals/patrick-cs4d/week5/mastermind.py
+++ practicals/patrick-cs4d/week5/mastermind.py
@@ -4,14 +4,9 @@ import random
 
 
 def make_4_digit_code():
-    code = ['_'] * 4
     numbers = ['1', '2', '3', '4', '5', '6']
     random.shuffle(numbers)
-    code[0] = numbers.pop()
-    code[1] = numbers.pop()
-    code[2] = numbers.pop()
-    code[3] = numbers.pop()
-    return code
+    return numbers[:4]
 
 
 def how_many_correct(guess, code):
@@ -51,11 +46,12 @@ if __name__ == '__main__':
         correct = how_many_correct(guess, code)
         part_correct = how_many_part_correct(guess, code)
 
-        turn = turn + 1
-        print('You have ' + str(correct) + ' in the right place and ' + str(part_correct) + ' in the wrong place \n')
+        print(f'You have {correct} in the right place and {part_correct} in the wrong place \n')
 
         if correct == 4: code_cracked = True
-        if turn == 11: break
+        if turn == 10: break
+
+        turn = turn + 1
 
     if code_cracked == True:
         print('Well Done')
