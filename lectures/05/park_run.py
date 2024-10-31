0fad0cfdaef841f63a9e16cb82fb14e91a2e4c29
Tony Jenkins <tony@tony-jenkins.co.uk>
2024-10-31 14:07:17 +0000

    Add fastest runner to Park Run

--- lectures/05/park_run.py
+++ lectures/05/park_run.py
@@ -7,20 +7,25 @@ def parse_line(data_line):
     return this_runner, int(this_time)
 
 
-def print_statistics(final_times):
+def print_statistics(final_times, best_runner):
 
     from statistics import mean
 
     print()
     print(f'Number of Runners: {len(final_times)}.')
+    print()
     print(f'Fastest Time:      {min(final_times)}.')
     print(f'Slowest Time:      {max(final_times)}.')
     print(f'Average Time:      {round(mean(final_times))}.')
+    print()
+    print(f'Fastest Time by Runner #{best_runner}.')
+    print()
 
 
 if __name__ == '__main__':
 
     times = []
+    top_runner = ''
 
     while True:
 
@@ -30,6 +35,15 @@ if __name__ == '__main__':
             break
         else:
             runner, current_time = parse_line(next_line)
+
+            if times:
+                top_runner = runner if current_time < min(times) else top_runner
+            else:
+                top_runner = runner
+
             times.append(current_time)
 
-    print_statistics(times)
+    if times:
+        print_statistics(times, top_runner)
+    else:
+        print('No data entered. Nothing to do.')
