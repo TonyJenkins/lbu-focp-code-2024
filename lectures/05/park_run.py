b0d1f562e1f4301975210239e3abf3637758d21b
Tony Jenkins <tony@tony-jenkins.co.uk>
2024-10-31 13:53:19 +0000

    Add basic stats in Park Run

--- lectures/05/park_run.py
+++ lectures/05/park_run.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python3
 
 
+def parse_line(data_line):
+    this_runner, this_time = data_line.split('::')
+
+    return this_runner, int(this_time)
+
+
+def print_statistics(final_times):
+    print()
+    print(f'Number of Runners: {len(final_times)}.')
+    print(f'Fastest Time:      {min(final_times)}.')
+    print(f'Slowest Time:      {max(final_times)}.')
+
+
 if __name__ == '__main__':
+
+    times = []
 
     while True:
 
@@ -9,3 +24,8 @@ if __name__ == '__main__':
 
         if next_line == 'END':
             break
+        else:
+            runner, current_time = parse_line(next_line)
+            times.append(current_time)
+
+    print_statistics(times)
